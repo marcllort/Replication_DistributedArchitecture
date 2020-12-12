b79e0d2b0ca4a68a18c895b32c500951f1d41311
Alex Almansa <alexalmansa5@gmail.com>
2020-12-12 11:20:15 +0100

    Html improved

--- web/js/main.js
+++ web/js/main.js
@@ -1,63 +1,57 @@
-var nodes = Object.freeze(['A1', 'A2', 'A3', 'B1', 'B2', 'C1', 'C2']);
+var layer_node = Object.freeze(['A1', 'A2', 'A3', 'B1', 'B2', 'C1', 'C2']);
 var keys = []
 
-function updateNodeValue(node_id, value) {
-    document.getElementById(node_id).innerHTML = value;
-}
-
-function findPreviousRowId(variable) {
-    let candidate = 0;
-    for (let index in keys) {
-        if (keys[index] < variable && keys[index] > candidate) {
-            candidate = keys[index];
+function previousId(variable) {
+    var candidate = 0;
+    for (let i in keys) {
+        if (keys[i] < variable && keys[i] > candidate) {
+            candidate = keys[i];
         }
     }
     return candidate === 0 ? null : "value-" + candidate;
 }
 
-function addRow(key) {
+function addKey(key) {
     str = "<tr id=\"value-" + key + "\"><td>" + key + "</td>";
-    for (let index in nodes) {
-        str += "<td id=\"" + nodes[index] + "-" + key + "\"></td>";
+    for (let i in layer_node) {
+        str += "<td id=\"" + layer_node[i] + "-" + key + "\"></td>";
     }
     str += "</tr>";
     return str;
 }
 
-function ready() {
+function load() {
     var ws = [];
     var initialPort = 4010;
 
-    for (let index in nodes) {
-        ws[index] = new WebSocket("ws://localhost:" + (initialPort + +index));
-        ws[index].onmessage = function (event) {
+    for (let i in layer_node) {
+        ws[i] = new WebSocket("ws://localhost:" + (initialPort + +i));
+        ws[i].onmessage = function (event) {
 
             let msg = event.data.split('-');
             if (msg.length === 1)
                 return;
 
-            let variable = msg[0];
+            let key = msg[0];
             let value = msg[1];
 
-            if (!keys.includes(variable)) {
-                let id = findPreviousRowId(variable);
-                console.log(id);
-                let row = addRow(variable);
+            if (!keys.includes(key)) {
+                let id = previousId(key);
+                let row = addKey(key);
                 if (id != null)
                     document.getElementById(id).insertAdjacentHTML('afterend', row);
                 else
-                    document.getElementById('table-header').insertAdjacentHTML('afterend', row);
-                keys.push(variable);
+                    document.getElementById('header_table').insertAdjacentHTML('afterend', row);
+                keys.push(key);
             }
 
-            console.log(nodes[index] + "-" + variable);
-            updateNodeValue(nodes[index] + "-" + variable, value);
+            document.getElementById(layer_node[i] + "-" + key).innerHTML = value;
         };
     }
 }
 
 document.onreadystatechange = function () {
     if (document.readyState == "interactive") {
-        ready()
+        load()
     }
 }
