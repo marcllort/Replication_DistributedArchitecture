eabd57f4846ed1435d8002df6bafc65be2e2cb6a
marcllort <mac12llm@gmail.com>
2020-12-12 12:21:14 +0100

    Working code

--- web/js/main.js
+++ web/js/main.js
@@ -1,57 +1,55 @@
-var layer_node = Object.freeze(['A1', 'A2', 'A3', 'B1', 'B2', 'C1', 'C2']);
-var keys = []
+const keys = [];
+const layer_nodes = ['A1', 'A2', 'A3', 'B1', 'B2', 'C1', 'C2'];
 
-function previousId(variable) {
-    var candidate = 0;
-    for (let i in keys) {
-        if (keys[i] < variable && keys[i] > candidate) {
-            candidate = keys[i];
-        }
+document.onreadystatechange = function () {
+    if (document.readyState === "interactive") {
+        load()
     }
-    return candidate === 0 ? null : "value-" + candidate;
 }
 
 function addKey(key) {
-    str = "<tr id=\"value-" + key + "\"><td>" + key + "</td>";
-    for (let i in layer_node) {
-        str += "<td id=\"" + layer_node[i] + "-" + key + "\"></td>";
+    let line = "<tr id=\"key-" + key + "\"><td id=\"A" + key + "-0\">" + key + "</td>";
+    for (let i in layer_nodes) {
+        line += "<td id=\"" + layer_nodes[i] + "-" + key + "\"></td>";
     }
-    str += "</tr>";
-    return str;
+    line += "</tr>";
+    return line;
+}
+
+function previousId(key) {
+    let newValue = 0;
+    for (let i in keys) {
+        if (keys[i] < key && keys[i] > newValue) {
+            newValue = keys[i];
+        }
+    }
+    if (newValue === 0) {
+        document.getElementById('header_table').insertAdjacentHTML('afterend', addKey(key));
+    } else {
+        document.getElementById("key-" + newValue).insertAdjacentHTML('afterend', addKey(key));
+    }
 }
 
 function load() {
-    var ws = [];
-    var initialPort = 4010;
+    const basePort = 4010;
+    const websockets = [];
 
-    for (let i in layer_node) {
-        ws[i] = new WebSocket("ws://localhost:" + (initialPort + +i));
-        ws[i].onmessage = function (event) {
-
+    for (let i in layer_nodes) {
+        let port = basePort + +i;
+        websockets[i] = new WebSocket("ws://localhost:" + (port));
+        websockets[i].onmessage = function (event) {
             let msg = event.data.split('-');
-            if (msg.length === 1)
+            if (msg.length === 1) {
+                console.error("Message not properly formatted!")
                 return;
-
-            let key = msg[0];
-            let value = msg[1];
-
-            if (!keys.includes(key)) {
-                let id = previousId(key);
-                let row = addKey(key);
-                if (id != null)
-                    document.getElementById(id).insertAdjacentHTML('afterend', row);
-                else
-                    document.getElementById('header_table').insertAdjacentHTML('afterend', row);
-                keys.push(key);
             }
 
-            document.getElementById(layer_node[i] + "-" + key).innerHTML = value;
+            let key = msg[0];
+            if (!keys.includes(key)) {
+                previousId(key);
+                keys.push(key);
+            }
+            document.getElementById(layer_nodes[i] + "-" + key).innerHTML = msg[1]; // value
         };
-    }
-}
-
-document.onreadystatechange = function () {
-    if (document.readyState == "interactive") {
-        load()
     }
 }
